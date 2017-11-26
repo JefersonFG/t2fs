40913db67a850e8cc6426e166d85d6ac40915f7d
jefersonfg <jefersonfguimaraes@gmail.com>
2017-11-26 16:21:03 -0200

    Atualizados testes unitários.

--- unit_tests/unit_tests.cpp
+++ unit_tests/unit_tests.cpp
@@ -19,13 +19,17 @@ extern "C"
 TEST(identify2, asserts_size) {
     int big_size = 500;
     char big_string[big_size];
+    big_string[0] = '\0';
 
     EXPECT_GE(identify2(big_string, big_size), 0);
+    EXPECT_FALSE(big_string[0] == '\0');
 
     int small_size = 10;
     char small_string[small_size];
+    small_string[0] = '\0';
 
     EXPECT_LT(identify2(small_string, small_size), 0);
+    EXPECT_TRUE(small_string[0] == '\0');
 }
 
 int main(int argc, char **argv) {
